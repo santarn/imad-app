d20c56e75a87f8214f955df07a2cc1cd087727ad
santarn <30868148+santarn@users.noreply.github.com>
2017-08-09 22:23:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-three', function (req, res) {
+  res.send("hello article three");
+});
+
+app.get('/article-two', function (req, res) {
+  res.send("hello article two");
+});
+
+app.get('/article-one', function (req, res) {
+  res.send("hello article one");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
