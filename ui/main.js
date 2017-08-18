caba4351c0464f18500e0fda6d23d809787480f3
santarn <30868148+santarn@users.noreply.github.com>
2017-08-18 21:31:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element=document.getElementById('hey');
 element.innerHTML="new value";
+var img=document.getElementById('ii');
+img.onclick=function(){
+    img.style.marginLeft='100px';
+    
+};
