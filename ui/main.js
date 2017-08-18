bd58122d71a6913a7752f081457d8da431b2e54a
santarn <30868148+santarn@users.noreply.github.com>
2017-08-18 21:36:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ var element=document.getElementById('hey');
 element.innerHTML="new value";
 var img=document.getElementById('ii');
 img.onclick=function(){
-    img.style.marginLeft='100px';
+   var interval=setInterval(moveright,50);
     
 };
