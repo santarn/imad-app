3f98a3f9c85f6affd2bd29ee142e03c755e4e64c
santarn <30868148+santarn@users.noreply.github.com>
2017-08-18 21:38:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,12 @@ console.log('Loaded!');
 var element=document.getElementById('hey');
 element.innerHTML="new value";
 var img=document.getElementById('ii');
+var marginLeft=0;
+function moveright()
+{
+    marginLeft=marginLeft+1;
+     img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function(){
    var interval=setInterval(moveright,50);
     
