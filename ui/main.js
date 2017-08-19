6c5582cfd7387f8b99f8d494c241ff913c9feeaa
santarn <30868148+santarn@users.noreply.github.com>
2017-08-19 20:10:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,3 +18,17 @@ button.onclick=function(){
    
     
 };
+
+var namel=document.getElementById('name');
+var subm=document.getElementById('sub');
+subm.onclick=function(){
+    var names=['name1','name2','name3','name4'];
+var list='';
+for(var i=0;i<names.length;i++)
+{
+    list+='<li>'+names[i]+'</li>';
+    var ul=document.getElementById('list');
+    ul.innerHTML=list;
+    
+}
+};
