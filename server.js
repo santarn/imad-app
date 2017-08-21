9e101a94e5ad508b034a72a519d585de0e591c76
santarn <30868148+santarn@users.noreply.github.com>
2017-08-21 22:03:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ var config=
 };
 var pool=new Pool(config);
 app.get('/articles/:articleNames', function (req, res) {
-    pool.query("SELECT * FROM article WHERE title='"+req.params.articleNames+"'",function(err,result){
+    pool.query("SELECT * FROM article WHERE title=$1",[req.params.articleNames],function(err,result){
     if(err){
            res.status(500).send(err.toString());
        }else{
