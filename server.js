1ae0862427724ec29c6e84703b41786cce283e5e
santarn <30868148+santarn@users.noreply.github.com>
2017-08-21 21:16:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/testdb',function(req,res){
        if(err){
            res.status(500).send(err.toString());
        }else{
-           res.send(JSON.stringify(result));
+           res.send(JSON.stringify(result.rows));
        }
    }); 
 });
