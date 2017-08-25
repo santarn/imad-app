6675ec533516578a61fcad9d9f398241007f9987
santarn <30868148+santarn@users.noreply.github.com>
2017-08-25 18:09:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,4 @@
-var button=document.getElementById('counter');
-button.onclick=function(){
-     var request=new XMLHttpRequest();
-    request.onreadystatechange=function(){
-      if(request.readyState==XMLHttpRequest.DONE)
-      {
-          if(request.status==200)
-          {var counter=request.responseText;
-               var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
-          }
-      }
-        
-    };
-    
-   request.open('GET','http://santoshjain2198.imad.hasura-app.io/counter',true);
-   request.send(null);
-   
-    
-};
+
 
 
 var subm=document.getElementById('sub');
@@ -27,22 +8,14 @@ subm.onclick=function(){
       if(request.readyState==XMLHttpRequest.DONE)
       {
           if(request.status==200)
-          { var names=request.responseText;
-          names=JSON.parse(names);
-var list='';
-for(var i=0;i<names.length;i++)
-{
-    list+='<li>'+names[i]+'</li>';
-    var ul=document.getElementById('list');
-    ul.innerHTML=list;
-    
+          { 
+              alert('data stored');
+         
 }
           }
-      }
+      
         
     };
-var namel=document.getElementById('name');
-var name=namel.value;    
    request.open('POST','http://santoshjain2198.imad.hasura-app.io/create-user',true);
    request.send(JSON.stringify({username:santosh,password:password}));
    
