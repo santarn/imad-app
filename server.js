382de6cc7b13469781de43cd855f082780b38736
santarn <30868148+santarn@users.noreply.github.com>
2017-08-25 15:41:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,10 @@ function hash(input,salt){
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
 }
-
+app.get('/hash/:input',function(req,res){
+    var hashvalue=hash(req.params.input,'heyhey');
+    res.send(hashvalue);
+});
 app.post('/create-user',function(res,req){
     var username = req.body.username;
     var password = req.body.password;
@@ -95,10 +98,7 @@ app.get('/articles/:articleNames', function (req, res) {
 
 
 
-app.get('/hash/:input',function(req,res){
-    var hashvalue=hash(req.params.input,'heyhey');
-    res.send(hashvalue);
-});
+
 
 app.get('/testdb',function(req,res){
    pool.query('SELECT * FROM test',function(err,result){
