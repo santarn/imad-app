af22627dd3048f99ababf97c2488ca153a2e38dc
santarn <30868148+santarn@users.noreply.github.com>
2017-08-25 14:03:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
 var crypto=require('crypto');
+var bodyParser=require('bodyParser');
 var app = express();
 app.use(morgan('combined')); 
-
+app.use(bodyParser.json());
 function createht(data)
 {
     var title=data.title;
@@ -77,10 +78,20 @@ function hash(input,salt){
     return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
 }
 
-app.get('/create-user/',function(res,req){
+app.post('/create-user',function(res,req){
+    var username=req.body.username;
+    var password=req.body.password;
+    
     var salt=crypto.getRandomBytes(128).toString('hex');
     var dbstring=hash(password,salt);
-    pool.query('INSERT INTO ""')
+    pool.query('INSERT INTO "user" VALUES($1,$2)',[username,dbstring],function(err,result){
+         if(err){
+           res.status(500).send(err.toString());
+       }else{
+           
+    res.send('user created successfully:'+username);
+       }
+    });
 });
 
 app.get('/hash/:input',function(req,res){
