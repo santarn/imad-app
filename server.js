2f88d4eb2e789e16edc921a0ac6bcfc2fbadb210
santarn <30868148+santarn@users.noreply.github.com>
2017-08-27 21:38:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,23 +131,20 @@ app.post('/login', function (req, res) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
-          if(result.rows.length===0)
-          {
-              res.send('invalid userid or password');
-          }
-          else
-          {
-              var dbstring=result.rows[0].password;
-              var salt=dbstring.split('$')[2];
-              var hashed=hash(passowrd,salt);
-           if(hashed===dbstring)
-           {
-              res.send("u are logged in");
-           }
-           else
-           {
-               res.send('invalid userid or password');
-           }
+          if (result.rows.length === 0) {
+              res.status(403).send('username/password is invalid');
+          } else {
+              // Match the password
+              var dbString = result.rows[0].password;
+              var salt = dbString.split('$')[2];
+              var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
+              if (hashedPassword === dbString) {
+            
+                res.send('credentials correct!');
+                
+              } else {
+                res.status(403).send('username/password is invalid');
+              }
           }
       }
    });
