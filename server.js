d5701faa74a49323dcccda44c35eef4aae1422e7
santarn <30868148+santarn@users.noreply.github.com>
2017-08-27 20:04:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,190 +1,127 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool=require('pg').Pool;
-var crypto=require('crypto');
+var Pool = require('pg').Pool;
+var crypto = require('crypto');
 var bodyParser = require('body-parser');
 
-var config={
-user:'santoshjain2198',
-    database:'santoshjain2198',
-    host:'db.imad.hasura-app.io',
-    port:'5432',
-    password:process.env.DB_PASSWORD
+var config = {
+    user: 'santoshjain2198',
+    database: 'santoshjain2198',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
 };
 
 var app = express();
-app.use(morgan('combined')); 
+app.use(morgan('combined'));
 app.use(bodyParser.json());
 
-function createht(data){
-    var title=data.title;
-    var heading=data.heading;
-    var date=data.date;
-    var content=data.content;
-var htmltemp=`
-<!DOCTYPE html>
-<html>
-     <head>
-        <title>
-            ${title}
-        </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-   <div class="cont">
-    <div>
-       <a href="/">Home</a>
-    </div>
-    <hr/>
-    <h1>${heading}</h1>
-    <div>
-        ${date.toDateString()}
-    </div>
-        
-    <div>
-        <p>${content}</p>
-    </div>
-    </div>
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
     
-   
-</html>
-
-`; return htmltemp}
-
-
+    var htmlTemplate = `
+    <html>
+      <head>
+          <title>
+              ${title}
+          </title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="/ui/style.css" rel="stylesheet" />
+      </head> 
+      <body>
+          <div class="container">
+              <div>
+                  <a href="/">Home</a>
+              </div>
+              <hr/>
+              <h3>
+                  ${heading}
+              </h3>
+              <div>
+                  ${date.toDateString()}
+              </div>
+              <div>
+                ${content}
+              </div>
+              <hr/>
+              <h4>Comments</h4>
+              <div id="comment_form">
+              </div>
+              <div id="comments">
+                <center>Loading comments...</center>
+              </div>
+          </div>
+          <script type="text/javascript" src="/ui/article.js"></script>
+      </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 
-function hash(input,salt){
-
-    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
+function hash (input, salt) {
+    // How do we create a hash?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
 }
 
 
-app.get('/hash/:input',function(req,res){
-    var hashvalue=hash(req.params.input,'heyhey');
-    res.send(hashvalue);
+app.get('/hash/:input', function(req, res) {
+   var hashedString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashedString);
 });
 
-app.post('/create-user',function(res,req){
-    //username,password
-    //JSON
-    //{"username":"santosh","password":"password"}
-    var username=req.body.username;
-    var password=req.body.password;
-    var salt=crypto.randomBytes(128).toString('hex');
-    var dbstring=hash(password,salt);
-    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)', [username,dbstring],function(err,result){
-         if(err){
-           res.status(500).send(err.toString());
-       }else{
-           res.send('user created successfully: '+username);
-       }
-    });
+app.post('/create-user', function (req, res) {
+   // username, password
+   // {"username": "tanmai", "password": "password"}
+   // JSON
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send('User successfully created: ' + username);
+      }
+   });
 });
 
-app.post('/login',function(res,req){
-    //username,password
-    //JSON
-    //{"username":"santosh","password":"password"}
-    var username= req.body.username;
-     var password=req.body.password;
-    pool.query('SELECT * FROM "user" WHERE username=$1', [username],function(err,result){
-         if(err){
-           res.status(500).send(err.toString());
-       }else{
-           if(result.rows.length===0)
-           {
-               res.status(403).send('forbidden');
-           }
-           else{
-           var pw=result.rows[0].password;
-           var salt=pw.split('$')[2];
-           var hashed=hash(password,salt);
-           if(hashed===pw)
-           {
-               alert('loged in');
-           }
-           else
-           {
-               alert('invalid password');
-           }
-       }
-           
-       }
-    });
+var pool = new Pool(config);
+
+
+
+app.get('/articles/:articleName', function (req, res) {
+  // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
+  pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
+    if (err) {
+        res.status(500).send(err.toString());
+    } else {
+        if (result.rows.length === 0) {
+            res.status(404).send('Article not found');
+        } else {
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+        }
+    }
+  });
 });
 
-var pool=new Pool(config);
-
-app.get('/articles/:articleNames', function (req, res) {
-    pool.query("SELECT * FROM article WHERE title=$1",[req.params.articleNames],function(err,result){
-    if(err){
-           res.status(500).send(err.toString());
-       }else{
-           if(result.rows.length===0)
-           {
-               res.status(404).send('no article');
-           }
-           else{
-           var articledata=result.rows[0];
-    res.send(createht(articledata));
-       }}    
-    });
-    
+app.get('/ui/:fileName', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
 });
 
 
-
-
-
-app.get('/testdb',function(req,res){
-   pool.query('SELECT * FROM test',function(err,result){
-       if(err){
-           res.status(500).send(err.toString());
-       }else{
-           res.send(JSON.stringify(result.rows));
-       }
-   }); 
-});
-
-var counter=0;
-app.get('/counter', function (req, res) {
-   counter=counter+1;
-    res.send(counter.toString());
-});
-
-var names=[];
-app.get('/submit',function(req,res){
-    var name=req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-    
-});
-
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
