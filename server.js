bb6bdc8673f3ce6a6014ed64d8894199f37120d4
santarn <30868148+santarn@users.noreply.github.com>
2017-08-27 22:09:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
     user: 'santoshjain2198',
@@ -16,6 +17,9 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret:'someRandomSecretValue',
+}));
 
 function createTemplate (data) {
     var title = data.title;
